0d752c5560031da709c97b8ec76db2b9fe665a2b
Bradford Hovinen <bradford.hovinen@tngtech.com>
2014-09-25 21:02:19 +0200

    Add KeyEventReceiver

---
+++ src/asteroids/KeyEventReceiver.java
@@ -0,0 +1,13 @@
+package asteroids;
+
+public interface KeyEventReceiver {
+    void upPressed();
+
+    void downPressed();
+
+    void leftPressed();
+
+    void rightPressed();
+
+    void spacePressed();
+}

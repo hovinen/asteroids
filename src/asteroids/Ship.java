7a349a79a2e9c6e852a03977be57bc1655b79970
Bradford Hovinen <bradford.hovinen@tngtech.com>
2014-09-25 20:25:09 +0200

    Add coordinates

--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -1,5 +1,14 @@
 package asteroids;
 
 public class Ship {
+    private double xCoordinate;
+    private double yCoordinate;
 
+    public double getxCoordinate() {
+	return xCoordinate;
+    }
+
+    public double getyCoordinate() {
+	return yCoordinate;
+    }
 }

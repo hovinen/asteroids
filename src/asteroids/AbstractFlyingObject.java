4728cb791f4cb3388150dcbb6e39264062b02c6e
Dragan Spirov <dragan.spirov@mgm-tp.com>
2014-09-25 20:47:32 +0200

    Extract common class

---
+++ src/asteroids/AbstractFlyingObject.java
@@ -0,0 +1,40 @@
+package asteroids;
+
+abstract class AbstractFlyingObject implements TickReceiver {
+    private double xCoordinate;
+    private double yCoordinate;
+
+    protected double xSpeed;
+    protected double ySpeed;
+
+    @Override
+    public void tick() {
+        xCoordinate = boundCoordinate(xCoordinate + xSpeed);
+        yCoordinate = boundCoordinate(yCoordinate + ySpeed);
+    }
+
+    private double boundCoordinate(double coordinate) {
+        if (coordinate > 1) {
+            coordinate -= Math.floor(coordinate);
+        }
+
+        if (coordinate < 0) {
+            coordinate -= Math.floor(coordinate);
+        }
+
+        return coordinate;
+    }
+
+    public void accelerate(double deltaX, double deltaY) {
+        xSpeed += deltaX;
+        ySpeed += deltaY;
+    }
+
+    public double getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public double getYCoordinate() {
+        return yCoordinate;
+    }
+}
--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -1,40 +1,12 @@
 package asteroids;
 
-public class Ship implements TickReceiver {
-    private double xCoordinate;
-    private double yCoordinate;
+public class Ship extends AbstractFlyingObject{
 
-    private double xSpeed;
-    private double ySpeed;
-
-    @Override
-    public void tick() {
-	xCoordinate = boundCoordinate(xCoordinate + xSpeed);
-	yCoordinate = boundCoordinate(yCoordinate + ySpeed);
-    }
-
-    private double boundCoordinate(double coordinate) {
-	if (coordinate > 1) {
-	    coordinate -= Math.floor(coordinate);
-	}
-
-	if (coordinate < 0) {
-	    coordinate -= Math.floor(coordinate);
-	}
-
-	return coordinate;
-    }
 
     public void accelerate(double deltaX, double deltaY) {
-	xSpeed += deltaX;
-	ySpeed += deltaY;
+	    xSpeed += deltaX;
+	    ySpeed += deltaY;
     }
 
-    public double getXCoordinate() {
-	return xCoordinate;
-    }
 
-    public double getYCoordinate() {
-	return yCoordinate;
-    }
 }

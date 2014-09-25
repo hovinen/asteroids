68010bea14a6b1ebc0649eb78aefbcc6d862c834
dpolivaev <dpolivaev>
2014-09-25 21:27:31 +0200

    main thread in the app

--- src/asteroids/App.java
+++ src/asteroids/App.java
@@ -8,8 +8,22 @@ public class App {
 			JFrame frame = new JFrame("youwillneverfigurethisout");
 			frame.setLayout(new BorderLayout());
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			Ship ship = new Ship();
-			JField field = new JField(ship);
+			final Ship ship = new Ship();
+			final JField field = new JField(ship);
+			final MainThread mainThread = new MainThread(new TickReceiver() {
+				
+				@Override
+				public void tick() {
+					SwingUtilities.invokeLater(new Runnable() {
+						@Override
+						public void run() {
+							ship.tick();
+							field.tick();
+						}
+					});
+				}
+			});
+			mainThread.start();
 			frame.getContentPane().add(field, BorderLayout.CENTER);
 			frame.setTitle("Asteroids");
 			frame.pack();

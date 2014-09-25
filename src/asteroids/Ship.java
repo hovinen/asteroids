bf6db58cba259e0e09cc7558aee233eb4db3815b
Bradford Hovinen <bradford.hovinen@tngtech.com>
2014-09-25 20:29:12 +0200

    Add speed + tick

--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -4,6 +4,14 @@ public class Ship {
     private double xCoordinate;
     private double yCoordinate;
 
+    private double xSpeed;
+    private double ySpeed;
+
+    public void tick() {
+	xCoordinate += xSpeed;
+	yCoordinate += ySpeed;
+    }
+
     public double getxCoordinate() {
 	return xCoordinate;
     }

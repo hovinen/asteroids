db0f740553f801478a2945a390f19c2450169dd6
Bradford Hovinen <bradford.hovinen@tngtech.com>
2014-09-25 21:04:05 +0200

    Fix conflict

--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -1,34 +1,8 @@
 package asteroids;
 
-public class Ship  extends AbstractFlyingObject{
-    private double xCoordinate = 0.5;
-    private double yCoordinate = 0.5;
-
-    private double xSpeed;
-    private double ySpeed;
-    
-    final private double size = 0.1;
-
-    @Override
-    public void tick() {
-	xCoordinate += xSpeed;
-	yCoordinate += ySpeed;
-    }
-
+public class Ship extends AbstractFlyingObject {
     public void accelerate(double deltaX, double deltaY) {
 	    xSpeed += deltaX;
 	    ySpeed += deltaY;
     }
-
-    public double getXCoordinate() {
-	return xCoordinate;
-    }
-
-    public double getYCoordinate() {
-	return yCoordinate;
-    }
-
-	public double getSize() {
-		return size;
-	}
 }

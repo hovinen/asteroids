8fb6701353ee6f0624e057bd42506212fc625ddc
Bradford Hovinen <bradford.hovinen@tngtech.com>
2014-09-25 21:24:05 +0200

    Create some asteroids

--- src/asteroids/Universe.java
+++ src/asteroids/Universe.java
@@ -1,9 +1,17 @@
 package asteroids;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Universe {
-    private List<Asteroid> asteroids;
+    private final List<Asteroid> asteroids = new ArrayList<>();
+
+    public Universe() {
+	asteroids
+		.add(new Asteroid(0.25, 0.25, 0.05).withVelocity(-0.01, -0.01));
+	asteroids
+		.add(new Asteroid(0.65, 0.15, 0.07).withVelocity(-0.015, 0.01));
+    }
 
     public Iterable<Asteroid> getAsteriods() {
 	return asteroids;

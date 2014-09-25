467720e62df9d5fb10e38ef7d27a858ea6cbfc95
dpolivaev <dpolivaev>
2014-09-25 21:32:06 +0200

    integrate key event processor

--- src/asteroids/App.java
+++ src/asteroids/App.java
@@ -1,5 +1,8 @@
 package asteroids;
 import java.awt.BorderLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
@@ -9,7 +12,35 @@ public class App {
 			frame.setLayout(new BorderLayout());
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			final Ship ship = new Ship();
+			final KeyEventProcessor keyEventProcessor = new KeyEventProcessor(ship);
 			final JField field = new JField(ship);
+			field.addKeyListener(new KeyAdapter() {
+
+				@Override
+				public void keyPressed(KeyEvent e) {
+					switch (e.getKeyCode()) {
+					case KeyEvent.VK_UP:
+						keyEventProcessor.upPressed();
+						break;
+					case KeyEvent.VK_DOWN:
+						keyEventProcessor.downPressed();
+						break;
+					case KeyEvent.VK_LEFT:
+						keyEventProcessor.leftPressed();
+						break;
+					case KeyEvent.VK_RIGHT:
+						keyEventProcessor.rightPressed();
+						break;
+					case KeyEvent.VK_SPACE:
+						keyEventProcessor.spacePressed();
+						break;
+
+					default:
+						break;
+					}
+				}
+				
+			});
 			final MainThread mainThread = new MainThread(new TickReceiver() {
 				
 				@Override

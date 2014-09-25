cf24b6abf55e335d37448256ab91e3b7042ec833
Dragan Spirov <dragan.spirov@mgm-tp.com>
2014-09-25 20:48:59 +0200

    Asteroid class

---
+++ src/asteroids/Asteroid.java
@@ -0,0 +1,7 @@
+package asteroids;
+
+/**
+ * Created by MGM\dspirov on 9/25/14.
+ */
+public class Asteroid extends AbstractFlyingObject {
+}
--- src/asteroids/Ship.java
+++ src/asteroids/Ship.java
@@ -2,11 +2,9 @@ package asteroids;
 
 public class Ship extends AbstractFlyingObject{
 
-
     public void accelerate(double deltaX, double deltaY) {
 	    xSpeed += deltaX;
 	    ySpeed += deltaY;
     }
-
 
 }
